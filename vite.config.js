894c7f87d184bb30ed037da682aef5e8f0e01cc2
Jeremy Cannell <cannell001@gannon.edu>
2025-11-15 17:15:17 -0500

    more than i asked for

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        tailwindcss()
-    ]
-})
+  // default config works with Tailwind via PostCSS
+});
+
